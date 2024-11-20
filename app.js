6991f6f66a3473fba7e0741a5f50a5c3051831f7
cem <cemyilmaztech@gmail.com>
2024-11-20 21:46:22 +0300

    get post put delete

--- app.js
+++ app.js
@@ -16,6 +16,54 @@ const PORT = process.env.PORT || 8000
 const HOST = process.env.HOST || '127.0.0.1'
 
 /* ------------------------------------------------------- */
+// app.get('/',(req, res)=> {
+
+//     // res.send('welcome to express')
+//     res.send({
+//         message: 'welcome to express'
+//     })
+    
+// })
+
+// app.post('/',(req, res)=> {
+
+//     // res.send('welcome to express')
+//     res.send({
+//         message: 'run post'
+//     })
+    
+// })
+
+// app.put('/',(req, res)=> {
+
+//     // res.send('welcome to express')
+//     res.send({
+//         message: 'run put'
+//     })
+    
+// })
+
+// app.delete('/',(req, res)=> {
+
+//     // res.send('welcome to express')
+//     res.send({
+//         message: 'run delete'
+//     })
+    
+// })
+
+
+app.all('/',(req, res)=> {
+
+    // res.send('welcome to express')
+    res.send({
+        message: 'run all'
+    })
+    
+})
+
+
+
 /* ------------------------------------------------------- */
 // app.listen(PORT, ()=> {console.log('Running: http://127.0.0.1:8000')})
 app.listen(PORT, ()=> {console.log(`Running: http://${HOST}:${PORT}`)})
