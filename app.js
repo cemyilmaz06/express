a10e264fcce31568e134a87fd52e9f088a42bcb0
cem <cemyilmaztech@gmail.com>
2024-11-20 21:06:10 +0300

    HOST AND PORT

--- app.js
+++ app.js
@@ -9,3 +9,13 @@
 const express = require('express')
 
 const app = express()
+
+require('dotenv').config()
+
+const PORT = process.env.PORT || 8000
+const HOST = process.env.HOST || '127.0.0.1'
+
+/* ------------------------------------------------------- */
+/* ------------------------------------------------------- */
+// app.listen(PORT, ()=> {console.log('Running: http://127.0.0.1:8000')})
+app.listen(PORT, ()=> {console.log(`Running: http://${HOST}:${PORT}`)})
